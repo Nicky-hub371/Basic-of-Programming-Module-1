9bd0d2cf2377201a34777c2fcd8d4b2ac47831f0
Ni.Xiong <xiong3ni@hotmail.com>
2023-09-05 19:16:43 +0800

    Initial commit

---
+++ PrinterApp.java
@@ -0,0 +1,53 @@
+
+public class PrinterApp {
+
+	public static void main(String[] args) {
+		// Printer App exercise
+		//Step 2
+		System.out.println("Printer App");
+		
+		//Step 3
+		System.out.println("This app is made by");
+		
+		//Step 4
+		String creator = "Ni.Xiong";
+		
+		//Step 5
+		System.out.println("This app is made by " + creator);
+		
+		//step 6
+		int number1 = 6;
+		int number2 = 3;
+		
+		//step 7
+		System.out.println(number1);
+		
+		//step 8
+		System.out.println("The value of number1 is " + number1);
+		
+		//step 9
+		System.out.println("The value of number2 is " + number2);
+		
+		//Step 10 and 11
+		int multiplication = number1 * number2;
+		
+		//Step 12
+		System.out.println(number1 + "*" +number2+"=" +multiplication);
+		
+		//Repeat step 10-12 for addistion
+		int add = number1 + number2;
+		System.out.println(number1+ "+" + number2+"=" + add);
+		
+		//Repeat step 10-12 for division
+		int division = number1 / number2;
+		System.out.println(number1+"/"+number2+"="+ division);
+		
+		//Repeat step 10-12 for subs
+		int subs = number1 - number2;
+		System.out.println(number1+ "-"+number2+"=" + subs);
+		
+		
+		
+	}
+
+}
